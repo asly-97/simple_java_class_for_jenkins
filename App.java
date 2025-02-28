4917839ef27776f3705be6455c83a3b80fb22d96
Abdeljabbar Elassali <115737234+asly-97@users.noreply.github.com>
2025-02-28 11:12:47 -0600

    New Changes

--- App.java
+++ App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) {
         
         System.out.println("Hello from Java App -- hosted on Github");
+        System.out.println("New Changes Added -- through GitHub");
 
     }
 }
